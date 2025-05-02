c1c77aac52cb0b2d7f30a0c0458675d94c58cc1f
ValeryChumpitaz <valery.chumpitaz@vallegrande.edu.pe>
2025-05-02 15:39:49 -0500

    Service

--- src/main/java/vallegrade/edu/pe/service/TaskService.java
+++ src/main/java/vallegrade/edu/pe/service/TaskService.java
@@ -5,7 +5,12 @@ import vallegrade.edu.pe.model.TaskEntity;
 import java.util.List;
 
 public class TaskService {
-
-
+    private TaskDAO = new TaskDAO();
+    public List<TaskEntity> obtenerTareas(){
+        return dao.listarTareas();
+    }
+    public void editarTarea(TaskEntity task){
+        dao.actualizarTarea(tarea);
+    }
 
 }
