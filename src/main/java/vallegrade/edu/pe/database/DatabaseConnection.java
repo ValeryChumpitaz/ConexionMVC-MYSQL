92c4dd8d8cfd997e205823e4e3c35c34009387f4
ValeryChumpitaz <valery.chumpitaz@vallegrande.edu.pe>
2025-05-02 15:59:09 -0500

    Database Conection

--- src/main/java/vallegrade/edu/pe/database/DatabaseConnection.java
+++ src/main/java/vallegrade/edu/pe/database/DatabaseConnection.java
@@ -5,7 +5,13 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
+ private static final String URL = "jdbc:mysql://localhost:3306/taskdb";
+ private static final String USER = "root";
+ private static final String PASSWORD = "72506286";
 
+ public static Connection getConnection() throws SQLException {
+     return DriverManager.getConnection(URL, USER, PASSWORD);
+ }
 }
 
 
