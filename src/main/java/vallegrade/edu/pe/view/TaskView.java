087669d766a77391434fcee5c2e5a269a138abfd
ValeryChumpitaz <valery.chumpitaz@vallegrande.edu.pe>
2025-05-02 16:15:59 -0500

    Update

--- src/main/java/vallegrade/edu/pe/view/TaskView.java
+++ src/main/java/vallegrade/edu/pe/view/TaskView.java
@@ -2,17 +2,20 @@ package vallegrade.edu.pe.view;
 import vallegrade.edu.pe.controller.TaskController;
 import vallegrade.edu.pe.model.TaskEntity;
 
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.List;
 
 
-public class TaskView extends JFrame {
 
+
+public class TaskView extends JFrame {
     private JTable tabla;
-    private DefaultTableModel model;
+    private DefaultTableModel modelo;
     private TaskController controller = new TaskController();
+
 
     public TaskView() {
         setTitle("Gestor de Tareas");
@@ -20,32 +23,42 @@ public class TaskView extends JFrame {
         setSize(600, 400);
         setLocationRelativeTo(null);
 
-        modelo = new DefaultTableModel(new String[]
-                {"ID", "Titulo", "Descripcion", "Estado"}, 0);
+
+        modelo = new DefaultTableModel(new String[]{"ID", "Título", "Descripción", "Estado"}, 0);
         tabla = new JTable(modelo);
         JScrollPane scroll = new JScrollPane(tabla);
-        JButton btnEditar = new JButton("Editar");
+
+
+        JButton btnEditar = new JButton("Editar tarea seleccionada");
         btnEditar.addActionListener(e -> editarTarea());
+
+
         add(scroll, BorderLayout.CENTER);
         add(btnEditar, BorderLayout.SOUTH);
+
+
         cargarDatos();
         setVisible(true);
     }
+
 
     private void cargarDatos() {
         modelo.setRowCount(0);
         List<TaskEntity> lista = controller.listar();
         for (TaskEntity t : lista) {
-            modelo.addRow(new Object[] {t.getId(), t.getTitulo(), t.getDescripcion(), t.getEstado()});
+            modelo.addRow(new Object[]{t.getId(), t.getTitulo(), t.getDescripcion(), t.getEstado()});
+        }
     }
-}
+
+
     private void editarTarea() {
         int fila = tabla.getSelectedRow();
-        if(fila != -1) {
+        if (fila != -1) {
             int id = (int) modelo.getValueAt(fila, 0);
-            String titulo = JOptionPane.showInputDialog("Nuevo titulo", modelo.getValueAt(fila, 1));
-            String descripcion = JOptionPane.showInputDialog("Nueva Descripcion:", modelo.getValueAt(fila, 2));
-            String estado = JOptionPane.showInputDialog("Nuevo Estado:", modelo.getValueAt(fila, 3));
+            String titulo = JOptionPane.showInputDialog("Nuevo título:", modelo.getValueAt(fila, 1));
+            String descripcion = JOptionPane.showInputDialog("Nueva descripción:", modelo.getValueAt(fila, 2));
+            String estado = JOptionPane.showInputDialog("Nuevo estado:", modelo.getValueAt(fila, 3));
+
 
             TaskEntity tarea = new TaskEntity();
             tarea.setId(id);
@@ -57,7 +70,7 @@ public class TaskView extends JFrame {
             controller.actualizar(tarea);
             cargarDatos();
         } else {
-            JOptionPane.showMessageDialog(this, "Selecciona una tarea para editar");
+            JOptionPane.showMessageDialog(this, "Selecciona una tarea para editar.");
         }
     }
 }
