cbcd63ec63ea91b56d32adc01dc396a306b5f6a3
ValeryChumpitaz <valery.chumpitaz@vallegrande.edu.pe>
2025-05-02 16:03:00 -0500

    Controller

--- src/main/java/vallegrade/edu/pe/controller/TaskController.java
+++ src/main/java/vallegrade/edu/pe/controller/TaskController.java
@@ -2,9 +2,22 @@ package vallegrade.edu.pe.controller;
 import vallegrade.edu.pe.model.TaskEntity;
 import vallegrade.edu.pe.service.TaskService;
 
+
 import java.util.List;
+
 
 public class TaskController {
 
 
+    private TaskService service = new TaskService();
+
+
+    public List<TaskEntity> listar() {
+        return service.obtenerTareas();
+    }
+
+
+    public void actualizar(TaskEntity tarea) {
+        service.editarTarea(tarea);
+    }
 }
--- src/main/java/vallegrade/edu/pe/service/TaskService.java
+++ src/main/java/vallegrade/edu/pe/service/TaskService.java
@@ -2,15 +2,24 @@ package vallegrade.edu.pe.service;
 import vallegrade.edu.pe.model.TaskDAO;
 import vallegrade.edu.pe.model.TaskEntity;
 
+
 import java.util.List;
 
+
 public class TaskService {
-    private TaskDAO = new TaskDAO();
-    public List<TaskEntity> obtenerTareas(){
+
+
+
+
+    private TaskDAO dao = new TaskDAO();
+
+
+    public List<TaskEntity> obtenerTareas() {
         return dao.listarTareas();
     }
-    public void editarTarea(TaskEntity task){
+
+
+    public void editarTarea(TaskEntity tarea) {
         dao.actualizarTarea(tarea);
     }
-
 }
